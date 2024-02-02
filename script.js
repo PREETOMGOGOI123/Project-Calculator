67d8487a7856cf5601f1cc77c70ff0c6dd68f877
PREETOMGOGOI123 <gogoi.preetom13@gmail.com>
2024-02-02 10:35:34 +0530

    new changes, seperating each function and storing as an object

--- script.js
+++ script.js
@@ -1,112 +1,136 @@
 
-const clearText = document.getElementById('clear');//gets the C button
+const clearText = document.getElementById('clear'); //gets the C button
 const allClearText = document.getElementById('all-clear');//gets AC button
-const opsList = document.querySelectorAll('.operations');// gets all the operator buttons
+
+const operators = document.querySelectorAll('.operator');// get all the operation elements.
+
 const numbers = document.querySelectorAll('.numbers');//get all the number buttons
-const result = document.getElementById('equates'); // gets the '=' button
+
+// selects the equal to button
+const resultEquals = document.getElementById('equates'); 
 const displayScreen = document.querySelector('.display');//gets the div where result will be displayed
 
-
-
 let letsCalculate = true;
-let clickCount = 0
-let displayEquation = []
-let resultCalc = [];
-const regex = /[+\-*/]/g;
+let inputNumber;
+
+const input={
+    num1 : [],
+    ops  : [],
+    num2 : []
+};
 
 
-    // function to update the numbers and operators
-    function updateInput(event){
-        inputVariable = `${event.target.id}`;
-        let inputVariableText = `${event.target.innerText}`;
-        if(displayEquation.length == 0){
-            if (inputVariable == 'operator'){
-                alert('click a no');
-                return;
-            }
-            else{
-                displayEquation = [... displayEquation, inputVariableText];
-                console.log(displayEquation);
-                display()
-            }
-        }
-        else {
-            if (inputVariable == 'operator'){
-            if (clickCount == 0){
-                displayEquation = [... displayEquation, inputVariableText];
-                console.log(displayEquation);
-                display();
-                clickCount++;
-            }
-            else {
-                let newArray = displayEquation.splice(0,displayEquation.length-1);
-                displayEquation = [...newArray,inputVariableText];
-                console.log(displayEquation);
-                clickCount ++;
-                display();
-            }
-          
-        }
-        else{
-                clickCount = 0;
-                displayEquation = [... displayEquation, inputVariableText];
-                console.log(displayEquation);
-                display();
-        }
+const operations = {
+    '+': function(num1,num2){
+       return parseInt(num1) + parseInt(num2);
+    },
+    '-': function(num1,num2){
+        return parseInt(num1) - parseInt(num2);
+    },
+    '*':function(num1,num2){
+        return parseInt(num1) * parseInt(num2);
+    },
+    '/': function(num1,num2){
+        return parseInt(num1) / parseInt(num2);
+    },
+}
+
+function resetInput(){
+    if(!letsCalculate){
+        input.num1 = [];
+        input.num2 = [];
+        input.ops = []; 
     }
-        
+
+    else {
+        letsCalculate;
+    }
+}
+
+function equates(){
+        let num1 = input.num1.join('');
+        let num2 = input.num2.join('');
+        let result = operations[input.ops[input.ops.length-1]](num1,num2);
+        console.log(result);
+        letsCalculate =false;
+        resetInput();
+        input.num1 = [...input.num1, result];
+}
+
+function captureNum(numValue){
+    if(!letsCalculate && input.num1.length == 0){
+        input.num1 = [...input.num1, parseInt(numValue)];
+        letsCalculate = true;
+        console.log(input)
+    
+    }
+    else if(!letsCalculate && input.num1.length > 0 ){
+        input.num1 = [];
+        input.num1 = [...input.num1, parseInt(numValue)];
+        letsCalculate = true;
+        console.log(input)
+    }
+
+    else if(input.ops.length == 0 && letsCalculate){
+        input.num1 = [...input.num1, parseInt(numValue)];
+        console.log(input)
     }
     
-    // function to introduce a new head element inside the display div
-    function display(){
-        displayScreen.innerHTML = `<h1>${displayEquation.join('')}</h1>`; 
-
+    else if(input.ops.length > 0 && letsCalculate && input.num2.length >= 0){
+        input.num2 = [...input.num2, parseInt(numValue)];
+        console.log(input)
     }
-
-    // function to reset everything;
-
-    function allClear(){
-            num = '';
-            opsValues = '';
-            displayEquation = [];
-            inputVariable = '';
-            resultCalc = [];
-        clickCount = 0;
-            display();
-    }
-
-    // function to clear everything
-
-    function clear(){
-            let newArray = displayEquation.splice(0,displayEquation.length-1);
-            displayEquation = [...newArray,inputVariableText];
-    }
-
-    // function to claculate the final result
-
-    function calculateFinalResult() {
-        let finalResult = displayEquation.join('');
-            displayResult = eval(finalResult);
-            displayEquation.push(` = ${displayResult}`);
-            display();
-            letsCalculate = false;
-    }
-
-    // Event Listner Capturing the numbers
-
-    numbers.forEach(function(numClick){
-        numClick.addEventListener('click',updateInput);
-    })
-
-    opsList.forEach(function(ops){
-        ops.addEventListener('click',updateInput);
-    })
-
-    //Event Listner capturing the all clear button
-    allClearText.addEventListener('click',allClear);
-
-    //Event Listner to capture the equal button
-    result.addEventListener('click', calculateFinalResult);
-
- 
     
+}
+
+function captureOps(opsValue){
+    if(!letsCalculate && input.num1.length == 0){
+        return;
+    }
+    else if(!letsCalculate && input.num1.length > 0){
+        input.ops = [...input.ops, opsValue];
+        letsCalculate = true;
+    }
+    else if(input.num1.length > 0 && input.num2.length > 0 && input.ops.length>0){
+           let num1 = input.num1.join('');
+           let num2 = input.num2.join('');
+           let result = operations[input.ops[input.ops.length-1]](num1,num2);
+           console.log(result);
+           letsCalculate =false;
+           resetInput();
+           input.num1 = [...input.num1, result];
+           input.ops = [...input.ops, opsValue];
+           console.log(input);
+           letsCalculate = true;
+    }
+        else if(input.num1.length >= 0 && letsCalculate){
+            input.ops = [...input.ops,opsValue];
+            console.log(input);
+        }
+    }
+
+
+numbers.forEach(numClick =>{
+    numClick.addEventListener('click',function(event){
+        const numValue =  event.target.textContent;
+        captureNum(numValue);
+    });//eventlistner when any number id clicked
+
+})
+
+operators.forEach(opsClick =>{
+        opsClick.addEventListener('click',function(){
+            const opsValue = this.getAttribute('data-operation');
+            captureOps(opsValue);
+        })
+    })
+
+allClearText.addEventListener('click',function(){
+    letsCalculate = false;
+    resetInput();
+    console.log(input);
+})
+
+resultEquals.addEventListener('click',function(){
+    equates();
+})
